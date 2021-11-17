b6af25fb6c270e45291a7711b93b7edd600671ac
“vinpaletteone”  <binsuzy95@gmail.com>
2021-11-18 01:07:52 +0900

    폴더 생성

---
+++ src/components/SignUp/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Input() {
+    return (
+        <>
+            <input type="text" placeholder="이름 입력"/>
+            <input type="text" placeholder="id 입력"/>
+            <input type="password" placeholder="pw 입력"/>
+            <input type="password" placeholder="pw 재입력"/>
+        </>
+    )
+}
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Input from '../components/SignUp/Input'
+
+export default function SignUp() {
+    return (
+        <div>
+            <Input />
+        </div>
+    )
+}
