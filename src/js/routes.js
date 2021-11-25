1f1603e6f475424f45e14036d74ebd332bf104c3
“vinpaletteone”  <binsuzy95@gmail.com>
2021-11-25 18:51:33 +0900

    routes 경로 추가

---
+++ src/js/routes.js
@@ -0,0 +1,7 @@
+export { default as Main } from "../pages/TabMenu/Main";
+export { default as Notice } from "../pages/TabMenu/Notice";
+export { default as Chat } from "../pages/TabMenu/Chat";
+export { default as Mypage } from "../pages/TabMenu/Mypage";
+export { default as Login } from "../pages/Account/Login";
+export { default as SignUp } from "../pages/Account/SignUp";
+export { default as Test } from "../pages/Test";
